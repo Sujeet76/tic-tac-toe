c561dcae3c68ebfe3b1d88226d91b05851b36ff4
Sujeet76 <126092124+Sujeet76@users.noreply.github.com>
2024-10-04 11:55:03 +0530

    refactor ♻️: Remove boilerplate, update imports, add dependencies, and create App component.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 40,
+          textAlign: 'center',
+          marginTop: 100,
+          color: '#000',
+        }}>
+        Text some text
+      </Text>
+    </View>
+  );
+};
+
+export default App;
